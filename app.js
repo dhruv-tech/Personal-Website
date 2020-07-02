38aa876fb8ad9cb27e6b6672e5a093ba3b0d2ca0
Dhruv <26849655+dhruv-tech@users.noreply.github.com>
2020-07-03 01:41:10 +0530

    Template ready

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set("views", "./views")
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', {heading: "Hello, World_", desc: "Hi, I’m Dhruv, currently a CS student at UBC and former intern at Tata Consultancy Services and MyGov India. I conceive, develop and design software systems."});
 });
 
 const port = 3303;
