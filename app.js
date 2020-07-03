36c6d69499309c20ce8625a38c69f023c05feb81
Dhruv <26849655+dhruv-tech@users.noreply.github.com>
2020-07-04 02:30:07 +0530

    skills section

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set("views", "./views")
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('home', {heading: "Hello, World_", desc: "Hi, I’m Dhruv, currently a CS student at UBC and former intern at Tata Consultancy Services and MyGov India. I conceive, develop and design software systems."});
+    res.render('home', {heading: "Hello, World_", desc: "I’m Dhruv Malik – an undergraduate at UBC, dedicated problem solver and software developer. I have specialized knowledge in application security & human-computer interaction."});
 });
 
 const port = 3303;
